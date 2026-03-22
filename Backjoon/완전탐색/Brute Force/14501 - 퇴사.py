19d39800d622ca776c4dbc0a46bdf50f7bfb8f8b
최영서 <choeyeongseo@choeyeongseoui-MacBookAir.local>
2026-03-23 02:20:08 +0900

    다른사람들의 풀이를 보고 벽을 느낀 문제. 단순히 일당 수입이 가장 큰 순부터 날짜 초과를 방지하는 방식(그리디)으로 풀었으나, 반례를 컨트롤 하지 못하고 틀림. 완전탐색을 어떻게 해야 효율적인가에 대한 고민을 많이 하게 된 문제. dfs를 이용하여 해당 날짜에 일을 할 수 있는지 검사한 뒤, 최고값을 기록해 논 뒤, 순회를 돌아오는 과정에서 최대값을 구하고 리턴하도록 함.

--- Backjoon/완전탐색/Brute Force/14501 - 퇴사.py
+++ Backjoon/완전탐색/Brute Force/14501 - 퇴사.py
@@ -1,57 +1,29 @@
 import sys
 
 n = int(sys.stdin.readline().strip())
-
-work = []
+T = []
+P = []
 for _ in range(n):
-    T, P = map(int, sys.stdin.readline().split())
-    work.append((T, P))
+    t, p = map(int, sys.stdin.readline().split())
+    T.append(t)
+    P.append(p)
 
-perIn = []
-for w in work:
-    (perIn.append(float(w[1] / w[0])))
-
-
-total = 0
-for _ in range(n):
-    # maxValue = max(perIn)
-    # maxIndex = [i for i, val in enumerate(perIn) if val == maxValue]
-    # if len(maxIndex) == 1:
-    #     maxIndex = maxIndex[0]
-    # else:
-    #     temp = []
-    #     for i in maxIndex:
-    #         temp.append((i, work[i][1]))
-    #         index, tempMaxValue = max(work)
-    #         maxIndex = index
-    maxValue = (
-        max(range(len(work)), key=lambda i: (work[i][1] / work[i][0], work[i][0])),
-    )
-
-    maxIndex = maxValue[0]
-
-    if perIn[maxIndex] == -1:
-        continue
-
-    if n < maxIndex + work[maxIndex][0]:
-        work[maxIndex] = (-1, -1)
-        perIn[maxIndex] = 0
-        continue
-    for i in range(maxIndex + 1, maxIndex + work[maxIndex][0] - 1):
-        if perIn[i] == -1:
-            work[maxIndex] = (-1, -1)
-            perIn[maxIndex] = 0
-
-    usable = 1
-    for i in range(maxIndex, maxIndex + work[maxIndex][0]):
-        if perIn[i] == -1:
-            usable = 0
-
-    if usable:
-        total += work[maxIndex][1]
-        for i in range(maxIndex, maxIndex + work[maxIndex][0]):
-            work[i] = (-1, -1)
-            perIn[i] = -1
+maxIncome = 0
 
 
-print(total)
+def dfs(day, income):
+    global maxIncome
+
+    if day > n:
+        return
+
+    if day == n:
+        maxIncome = max(maxIncome, income)
+        return
+
+    dfs(day + T[day], income + P[day])
+    dfs(day + 1, income)
+
+
+dfs(0, 0)
+print(maxIncome)
